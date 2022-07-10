539f20923c5542b16789c80cc9185c637426e7f9
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2022-07-10 18:12:57 -0300

    selecionando dados

---
+++ 6 - Consultando dados/selecionando_colunas.sql
@@ -0,0 +1,7 @@
+SELECT id_curso, nome_curso, resumo FROM tb_cursos;
+/* SELECT atributos_desejados FROM nome_da_tabela;
+
+/*
+O caracter especial * retorna todos os elementos exemplo:
+SELECT * FROM tb_cursos;
+*/
